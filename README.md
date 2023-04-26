8a64a9b1356634e8d96b7d8df0ab2e5912c54ab1
Swayamgour <119596165+Swayamgour@users.noreply.github.com>
2023-04-26 21:45:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-navbar
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vbeug8)
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+        <img  className = " nav_img2" style={{width:"60px" , height:'50px' }} src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTamStjvJZAmUPEIeGMximoV7ivlc0tcOtVfg&usqp=CAU"/>
+        <img  className = " nav_img3" style={{width:"60px" , height:'50px' }} src = "https://www.clipartmax.com/png/full/36-365828_navbar-toggle-icon-menu-hamburger-png-white.png"/>
+     <div className = " nav">
+       <div className = " nav_left">
+       <img  className = " nav_img" style={{width:"60px" , height:'50px' }} src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTamStjvJZAmUPEIeGMximoV7ivlc0tcOtVfg&usqp=CAU"/>
+
+     
+       <h3>Home
+         </h3>
+       <h3>About
+         </h3>
+       <h3>contact us
+         </h3>
+       <h3>Home
+         </h3>
+         </div>
+        
+         <div className = " nav_right">
+       <h3>Login
+         </h3>
+       <h3>Singup
+         </h3>
+         </div>
+        
+       </div>
+       
+       <div className="center">
+       <h5> totally free</h5>
+       <h2 style={{marginTop:'1rem'}}>
+         Let's your Team to Click
+         </h2>
+       <h2 style={{marginTop:'.5rem'}}>
+         Let's ykwek fnwnenjke 
+         </h2>
+         <h5 style={{marginTop:'2rem'}}  >
+         Complete aeronautical information about Franklin 
+           </h5>
+           <img className="imgleft" src="https://5.imimg.com/data5/KQ/SW/HX/SELLER-83695523/male-model-portfolio-service-500x500.jpg"/>
+       </div>
+    </div>
+  );
+}
